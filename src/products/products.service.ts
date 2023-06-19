ed27c548abb309ffdac2ba8c98f00858abcd6166
goncalveshigino <goncalveshigino20@gmail.com>
2023-06-19 13:45:29 +0100

    chore: mrthods findOne and Remove

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -32,20 +32,29 @@ export class ProductsService {
 
   }
 
-  findAll() {
-    return `This action returns all products`;
+  // TODO: paginar
+  findAll() { 
+    return this.productRepository.find({})
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne( id: string ) {
+
+    const product = await this.productRepository.findOneBy({ id });
+
+    if ( !product )
+       throw new NotFoundException(`Product with id ${ id } not found`);
+
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove( id: string ) {
+    const product = await this.findOne( id );
+
+    await this.productRepository.remove( product )
   }
 
   private handleDBExceptions( error: any ) {
