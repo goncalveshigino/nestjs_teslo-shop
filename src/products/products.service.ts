9ac5d2ea7721015ff3cfbe0be40553a3885822bc
goncalveshigino <goncalveshigino20@gmail.com>
2023-06-19 15:00:14 +0100

    chore: queryBuilder

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -59,10 +59,14 @@ export class ProductsService {
     if ( isUUID(term) ) {
       product = await this.productRepository.findOneBy({ id: term })
     } else {
-      product = await this.productRepository.findOneBy({ slug: term })
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product =  await queryBuilder
+        .where(`UPPER(title) =:title or slug =:slug`, {
+          title: term.toUpperCase(), 
+          slug: term.toLowerCase(),
+        }).getOne();
     }
 
-    //const product = await this.productRepository.findOneBy({ id });
 
     if ( !product )
        throw new NotFoundException(`Product with id ${ term } not found`);
