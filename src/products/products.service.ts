8b332e1c9f47b9186e5a81701b8c96856e3854fe
goncalveshigino <goncalveshigino20@gmail.com>
2023-06-19 11:20:14 +0100

    chore:Manejado exceptions

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,6 +7,8 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+
+  private logger = new Logger('ProductService');
 
   constructor(
 
@@ -25,8 +27,7 @@ export class ProductsService {
       return product;
       
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('Ajuda')
+      this.handleDBExceptions(error)
     }
 
   }
@@ -46,4 +47,16 @@ export class ProductsService {
   remove(id: number) {
     return `This action removes a #${id} product`;
   }
+
+  private handleDBExceptions( error: any ) {
+
+    if (error.code === '23505' )
+      throw new BadRequestException(error.detal);
+      
+    this.logger.error(error);
+
+    throw new InternalServerErrorException('Unexpected error, check server logs');
+
+  }
+  
 }
