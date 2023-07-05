9fc59f72409ea39ab1d2e1bd51cdfdbc6742afd0
goncalveshigino <goncalveshigino20@gmail.com>
2023-07-05 21:16:33 +0100

    chore:Entidades de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
 
 
 
@@ -35,7 +36,7 @@ import { FilesModule } from './files/files.module';
     ProductsModule, 
     CommonModule, 
     SeedModule, 
-    FilesModule,
+    FilesModule, AuthModule,
   ],
 
 })
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,32 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+
+
+@Entity('users')
+export class User {
+    
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    
+    @Column('text',{
+        unique: true
+    })
+    email: string;
+    
+    @Column('text')
+    password: string;
+    
+    @Column('text')
+    fullName: string;
+    
+    @Column('bool')
+    isActive: boolean; 
+    
+    @Column('text',{
+        array: true, 
+        default: ['user']
+    })
+    roles: string[];
+
+
+}
