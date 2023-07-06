5bf44a2712a49c50923992011b76ef0a6daf1058
goncalveshigino <goncalveshigino20@gmail.com>
2023-07-06 12:15:40 +0100

    chore:Authentication - Passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService], 
   imports: [
-    TypeOrmModule.forFeature([ User ])
+    TypeOrmModule.forFeature([ User ]), 
+
+    PassportModule.register({ defaultStrategy: 'jwt'}), 
+
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET, 
+    //   signOptions: {
+    //     expiresIn:'2h'
+    //   }
+    // })
+
+
   ], 
   exports: [ TypeOrmModule ]
 })
